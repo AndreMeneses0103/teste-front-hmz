732a8cf2c77141129842cbc77fec8c5f85d36034
andremeneses0103 <andremeneses0103@gmail.com>
2024-04-22 08:19:55 -0300

    Refactor

--- front-hmz/src/App.jsx
+++ front-hmz/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-// import "./App.css";
 import { Home } from "@/pages/Home";
 import { Users } from "./pages/Users";
 
